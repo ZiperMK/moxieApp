7e02f5f3c4427acb2b734cb99bb7bee31799921d
ZiperMK <stefan.mzt.stojkovski@gmail.com>
2022-05-14 15:55:15 +0200

    Project Base

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', 'codeigniter');
+   define('DB_PASSWORD', 'codeigniter2019');
+   define('DB_DATABASE', 'SISIII2022_89201017');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
